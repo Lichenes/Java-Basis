56cd7efbb34804e631fc02fe4777e9145351c926
Lichenes <1693597936@qq.com>
2019-07-05 10:14:39 +0800

    Hello

---
+++ src/com/jgs1902/test01/FristClass.java
@@ -0,0 +1,8 @@
+package com.jgs1902.test01;
+
+public class FristClass {
+	public static void main(String[] args){
+		System.out.println("我的第一个Java类!");
+		System.out.println("Hello World!");
+	}
+}
