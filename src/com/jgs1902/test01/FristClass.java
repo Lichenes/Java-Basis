613e959272f4a430408a1941263363d8a0b770e3
Lichenes <1693597936@qq.com>
2019-07-05 17:31:12 +0800

    基本数据类型初始值

--- src/com/jgs1902/test01/FristClass.java
+++ src/com/jgs1902/test01/FristClass.java
@@ -1,8 +1,20 @@
 package com.jgs1902.test01;
 
 public class FristClass {
+	
+	static byte a; //byte = 0
+	static short b; //short = 0
+	static int c; //int = 0
+	static long d; //long = 0L
+	static float e; //float = 0.0f
+	static double f; //double = 0.0d
+	static char g; //
+	static boolean h; // boolan = false
+	
 	public static void main(String[] args){
 		System.out.println("我的第一个Java类!");
 		System.out.println("Hello World!");
-	}
+		
+		System.out.println("byte = "+a+" short = "+b+" int = "+c+" long = "+d+" float = "+e+" double = "+f+" char = "+g+" boolean = "+h);
+	} 
 }
